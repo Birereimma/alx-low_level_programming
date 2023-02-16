6799e4db13399c23f758cb36699622e4b28b8426
Birereimma <immabirere@gmail.com>
2023-02-16 07:32:59 -0800

    a C program that prints exactly

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -a C program that prints a line to the  standard error
+ *
+ * Return 1 (Successful)
+ */
+int main(void)
+{
+	put("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+		return (1);
+}
