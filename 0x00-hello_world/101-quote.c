56871727cf70a788d618015822f31e518f14d183
Birereimma <immabirere@gmail.com>
2023-02-17 00:06:07 -0800

    a C program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main -a C program that prints a line to the  standard error
  *
- * Return 1 (Successful)
+ * Return: 1 (Successful)
  */
 int main(void)
 {
-	put("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 		return (1);
 }
