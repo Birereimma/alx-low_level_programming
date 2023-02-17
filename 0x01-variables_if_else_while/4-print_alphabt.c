7399aa158c5f3f97e18b3fb9ebbedd3c724fa8dc
Birereimma <immabirere@gmail.com>
2023-02-17 10:16:35 -0800

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,9 +14,11 @@ int  main(void)
 	while
 		(c <= 'z') {
 if ((c != 'q' && c != 'e') 
-				&& c <= 'z')
-			putchar(c);
-			c++;
+		&& c <= 'z')
+	putchar(c);
+
+c++;
 		}
+	putchar('\n');
 			return (0);
 }
