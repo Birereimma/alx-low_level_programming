dc9024b113351c24b25784a35bf85a57a3ae0216
Birereimma <immabirere@gmail.com>
2023-02-23 03:16:11 -0800

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,26 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
+    
+    if (a > b && a > c)
+    {
+	    largest = a;
+    }
+    
+    else if (a > b && c > a)
+    {
+	    largest = c;
+    }
 
-    if (a > b && b > c)
+    else if (b > c)
     {
-        largest = a;
+	    largest = b;
     }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
+    
     else
     {
-        largest = c;
+	    largest = c;
     }
-
     return (largest);
 }
