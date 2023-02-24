2b1e26398842beafe4f66af79238e4e26999c6f1
Birereimma <immabirere@gmail.com>
2023-02-24 05:21:53 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
-#include <stdio>
+#include "main.h"
+#include <stdio.h>
 
 /**
  * main - print the numbers from 1 to 100 followed bu \n
@@ -11,19 +12,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100, i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (i == 1)
 		{
