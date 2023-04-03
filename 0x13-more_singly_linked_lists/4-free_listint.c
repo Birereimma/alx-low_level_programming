9f784d7f12f67256a16c6c6fb266fb9524d88ef1
Birereimma <immabirere@gmail.com>
2023-04-03 06:39:32 -0700

    free memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+}
